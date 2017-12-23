f499348117803c80e50a3491de6a614dbfc071df
Aidan <aidanjacobhahn@gmail.com>
2017-12-23 11:35:43 -0800

    sketch of application workflow

--- src/apishell/core.clj
+++ src/apishell/core.clj
@@ -1,7 +1,15 @@
 (ns apishell.core
+  (:require [clj-http.client :as client])
+  (:require [clojure.java.io :as io] )
   (:gen-class))
+
+(defn parse-args-and-config
+  "take in link and content via arguments, modify link and content based on configurations in .apishell"
+  [link content]
+  ())
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ())
